b4eea22f3f56625873e5eb04d8dbd731d63fe61c
Bhuvanesh <bhuvaneshnick@gmail.com>
2017-11-19 21:55:35 +0530

    s

--- temp00/portfolio/birthday.js
+++ temp00/portfolio/birthday.js
@@ -5,9 +5,9 @@
 		$(".se-pre-con").fadeOut("slow");;
 	});
 
-var vbi_m=document.getElementsByClassName('bi_m');
+var vbi_m=document.getElementsByClassName('mg');
 
-var vbi_d=document.getElementsByClassName('bi_d');
+var vbi_d=document.getElementsByClassName('con');
 var vbi_over=document.getElementsByClassName('bi_over');
 var vbi_close=document.getElementsByClassName('bi_close');
 
@@ -15,34 +15,39 @@ function bi_fun(n,q){
     if(q==0){
   
     vbi_over[0].style.display='flex'; 
-    vbi_d[n].className='ex bi_d';
+    vbi_d[n].className='ex con';
 }
     if(q==1){
           vbi_over[0].style.display='flex'; 
-    vbi_d[n].className='ex_p bi_d';
+    vbi_d[n].className='ex_p con';
     }
 }
 
 function bi_hide(){
     vbi_over[0].style.display='none'; 
-    vbi_d[0].className='ptp bi_d';
-     vbi_d[1].className='ptl bi_d';
-     vbi_d[2].className='ptl bi_d';
-     vbi_d[3].className='ptl bi_d';
-     vbi_d[4].className='ptl bi_d';
-     vbi_d[5].className='ptp bi_d';
-       vbi_d[6].className='ptl bi_d';
-     vbi_d[7].className='ptl bi_d';
-     vbi_d[8].className='ptp bi_d';
-     vbi_d[9].className='ptp bi_d';
-     vbi_d[10].className='ptl bi_d';
-     vbi_d[11].className='ptl bi_d';
-       vbi_d[12].className='ptl bi_d';
-     vbi_d[13].className='ptl bi_d';
-     vbi_d[14].className='ptp bi_d';
-     vbi_d[15].className='ptl bi_d';
-     vbi_d[16].className='ptl bi_d';
-     vbi_d[17].className='ptp bi_d';
+    vbi_d[0].className='ptp con';
+     vbi_d[1].className='ptl con';
+     vbi_d[2].className='ptl con';
+     vbi_d[3].className='ptl con';
+     vbi_d[4].className='ptl con';
+     vbi_d[5].className='ptp con';
+       vbi_d[6].className='ptl con';
+     vbi_d[7].className='ptl con';
+     vbi_d[8].className='ptp con';
+     vbi_d[9].className='ptp con';
+     vbi_d[10].className='ptl con';
+     vbi_d[11].className='ptl con';
+       vbi_d[12].className='ptl con';
+     vbi_d[13].className='ptl con';
+     vbi_d[14].className='ptp con';
+     vbi_d[15].className='ptl con';
+     vbi_d[16].className='ptl con';
+     vbi_d[17].className='ptp con';
+      vbi_d[18].className='ptl con';
+      vbi_d[19].className='ptl con';
+    vbi_d[20].className='ptl con';
+    vbi_d[21].className='ptl con';
+    vbi_d[22].className='ptl con';  vbi_d[23].className='ptl con';
 
     
     
@@ -67,5 +72,11 @@ vbi_m[14].addEventListener('click',function(){bi_fun(14,1);}, false);
 vbi_m[15].addEventListener('click',function(){bi_fun(15,0);}, false);
 vbi_m[16].addEventListener('click',function(){bi_fun(16,0);}, false);
 vbi_m[17].addEventListener('click',function(){bi_fun(17,1);}, false);
+vbi_m[18].addEventListener('click',function(){bi_fun(18,0);}, false);
+vbi_m[19].addEventListener('click',function(){bi_fun(19,0);}, false);
+vbi_m[20].addEventListener('click',function(){bi_fun(20,0);}, false);
+vbi_m[21].addEventListener('click',function(){bi_fun(21,0);}, false);
+vbi_m[22].addEventListener('click',function(){bi_fun(22,0);}, false);
+vbi_m[23].addEventListener('click',function(){bi_fun(23,0);}, false);
 
 
