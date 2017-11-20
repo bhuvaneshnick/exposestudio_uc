96b0855669b88a210dc2e047b5ea537282727a5e
Bhuvanesh <bhuvaneshnick@gmail.com>
2017-11-21 00:59:58 +0530

    sd

--- temp00/portfolio/love_and_life.js
+++ temp00/portfolio/love_and_life.js
@@ -25,57 +25,58 @@ function bi_fun(n,q){
 
 function bi_hide(){
     vbi_over[0].style.display='none'; 
-    vbi_d[0].className='ptp con';
-     vbi_d[1].className='ptp con';
-     vbi_d[2].className='ptp con';
-     vbi_d[3].className='ptp con';
-     vbi_d[4].className='ptp con';
-     vbi_d[5].className='ptp con';
+    vbi_d[0].className='ptl con';
+     vbi_d[1].className='ptl con';
+     vbi_d[2].className='ptl con';
+     vbi_d[3].className='ptl con';
+     vbi_d[4].className='ptl con';
+     vbi_d[5].className='ptl con';
        vbi_d[6].className='ptl con';
      vbi_d[7].className='ptl con';
-     vbi_d[8].className='ptp con';
-     vbi_d[9].className='ptp con';
-     vbi_d[10].className='ptp con';
+     vbi_d[8].className='ptl con';
+     vbi_d[9].className='ptl con';
+     vbi_d[10].className='ptl con';
      vbi_d[11].className='ptl con';
        vbi_d[12].className='ptl con';
      vbi_d[13].className='ptl con';
      vbi_d[14].className='ptl con';
-     vbi_d[15].className='ptp con';
+     vbi_d[15].className='ptl con';
      vbi_d[16].className='ptl con';
-     vbi_d[17].className='ptp con';
+     vbi_d[17].className='ptl con';
       vbi_d[18].className='ptl con';
       vbi_d[19].className='ptl con';
-    vbi_d[20].className='ptl con';
-    vbi_d[21].className='ptl con';
-    vbi_d[22].className='ptl con';  vbi_d[23].className='ptl con';
+    vbi_d[20].className='ptp con';
+    vbi_d[21].className='ptp con';
+    vbi_d[22].className='ptl con';  
+    vbi_d[23].className='ptl con';
 
     
     
 }
 
 vbi_close[0].addEventListener('click',function(){bi_hide();}, false);
-vbi_m[0].addEventListener('click',function(){bi_fun(0,1);}, false);
-vbi_m[1].addEventListener('click',function(){bi_fun(1,1);}, false);
-vbi_m[2].addEventListener('click',function(){bi_fun(2,1);}, false);
-vbi_m[3].addEventListener('click',function(){bi_fun(3,1);}, false);
-vbi_m[4].addEventListener('click',function(){bi_fun(4,1);}, false);
-vbi_m[5].addEventListener('click',function(){bi_fun(5,1);}, false);
+vbi_m[0].addEventListener('click',function(){bi_fun(0,0);}, false);
+vbi_m[1].addEventListener('click',function(){bi_fun(1,0);}, false);
+vbi_m[2].addEventListener('click',function(){bi_fun(2,0);}, false);
+vbi_m[3].addEventListener('click',function(){bi_fun(3,0);}, false);
+vbi_m[4].addEventListener('click',function(){bi_fun(4,0);}, false);
+vbi_m[5].addEventListener('click',function(){bi_fun(5,0);}, false);
 vbi_m[6].addEventListener('click',function(){bi_fun(6,0);}, false);
 vbi_m[7].addEventListener('click',function(){bi_fun(7,0);}, false);
-vbi_m[8].addEventListener('click',function(){bi_fun(8,1);}, false);
-vbi_m[9].addEventListener('click',function(){bi_fun(9,1);}, false);
-vbi_m[10].addEventListener('click',function(){bi_fun(10,1);}, false);
+vbi_m[8].addEventListener('click',function(){bi_fun(8,0);}, false);
+vbi_m[9].addEventListener('click',function(){bi_fun(9,0);}, false);
+vbi_m[10].addEventListener('click',function(){bi_fun(10,0);}, false);
 vbi_m[11].addEventListener('click',function(){bi_fun(11,0);}, false);
 vbi_m[12].addEventListener('click',function(){bi_fun(12,0);}, false);
 vbi_m[13].addEventListener('click',function(){bi_fun(13,0);}, false);
 vbi_m[14].addEventListener('click',function(){bi_fun(14,0);}, false);
-vbi_m[15].addEventListener('click',function(){bi_fun(15,1);}, false);
+vbi_m[15].addEventListener('click',function(){bi_fun(15,0);}, false);
 vbi_m[16].addEventListener('click',function(){bi_fun(16,0);}, false);
-vbi_m[17].addEventListener('click',function(){bi_fun(17,1);}, false);
+vbi_m[17].addEventListener('click',function(){bi_fun(17,0);}, false);
 vbi_m[18].addEventListener('click',function(){bi_fun(18,0);}, false);
 vbi_m[19].addEventListener('click',function(){bi_fun(19,0);}, false);
-vbi_m[20].addEventListener('click',function(){bi_fun(20,0);}, false);
-vbi_m[21].addEventListener('click',function(){bi_fun(21,0);}, false);
+vbi_m[20].addEventListener('click',function(){bi_fun(20,1);}, false);
+vbi_m[21].addEventListener('click',function(){bi_fun(21,1);}, false);
 vbi_m[22].addEventListener('click',function(){bi_fun(22,0);}, false);
 vbi_m[23].addEventListener('click',function(){bi_fun(23,0);}, false);
 
